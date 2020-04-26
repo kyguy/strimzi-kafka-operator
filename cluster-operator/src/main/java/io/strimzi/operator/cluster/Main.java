c702c0b695b69a5c78fa9420cd363dd58eea4d66
Paolo Patierno <ppatierno@live.com>
2020-04-26 13:34:15 +0200

    Added KafkaClusterRebalance operator Added Cruise Control API
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/Main.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/Main.java
@@ -17,6 +17,7 @@ import io.strimzi.operator.cluster.operator.assembly.KafkaConnectAssemblyOperato
 import io.strimzi.operator.cluster.operator.assembly.KafkaConnectS2IAssemblyOperator;
 import io.strimzi.operator.cluster.operator.assembly.KafkaMirrorMakerAssemblyOperator;
 import io.strimzi.operator.cluster.operator.assembly.KafkaMirrorMaker2AssemblyOperator;
+import io.strimzi.operator.cluster.operator.assembly.KafkaClusterRebalanceAssemblyOperator;
 import io.strimzi.operator.cluster.operator.resource.ResourceOperatorSupplier;
 import io.strimzi.operator.common.PasswordGenerator;
 import io.strimzi.operator.common.operator.resource.ClusterRoleOperator;
@@ -126,6 +127,9 @@ public class Main {
         KafkaBridgeAssemblyOperator kafkaBridgeAssemblyOperator =
                 new KafkaBridgeAssemblyOperator(vertx, pfa, certManager, passwordGenerator, resourceOperatorSupplier, config);
 
+        KafkaClusterRebalanceAssemblyOperator kafkaClusterRebalanceAssemblyOperator =
+                new KafkaClusterRebalanceAssemblyOperator(vertx, pfa, resourceOperatorSupplier);
+
         List<Future> futures = new ArrayList<>();
         for (String namespace : config.getNamespaces()) {
             Promise<String> prom = Promise.promise();
@@ -139,6 +143,7 @@ public class Main {
                     kafkaMirrorMakerAssemblyOperator,
                     kafkaMirrorMaker2AssemblyOperator,
                     kafkaBridgeAssemblyOperator,
+                    kafkaClusterRebalanceAssemblyOperator,
                     resourceOperatorSupplier.metricsProvider);
             vertx.deployVerticle(operator,
                 res -> {
