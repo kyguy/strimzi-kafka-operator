be031b2e2311ac8aa8238f88520d18a90e138503
Paolo Patierno <ppatierno@live.com>
2020-05-01 10:08:02 +0200

    Added skip hard goal check flag in the rebalance resource
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- api/src/main/java/io/strimzi/api/kafka/model/KafkaClusterRebalanceSpec.java
+++ api/src/main/java/io/strimzi/api/kafka/model/KafkaClusterRebalanceSpec.java
@@ -23,13 +23,14 @@ import java.util.Map;
         builderPackage = "io.fabric8.kubernetes.api.builder"
 )
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "goals", "verbose" })
+@JsonPropertyOrder({ "goals", "skipHardGoalCheck", "verbose" })
 @EqualsAndHashCode
 public class KafkaClusterRebalanceSpec implements UnknownPropertyPreserving, Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private List<String> goals;
+    private boolean skipHardGoalCheck;
     private boolean verbose = false;
 
     private Map<String, Object> additionalProperties = new HashMap<>(0);
@@ -43,6 +44,16 @@ public class KafkaClusterRebalanceSpec implements UnknownPropertyPreserving, Ser
 
     public void setGoals(List<String> goals) {
         this.goals = goals;
+    }
+
+    @Description("Whether allow hard goals be skipped in proposal generation. Default is false.")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public boolean isSkipHardGoalCheck() {
+        return skipHardGoalCheck;
+    }
+
+    public void setSkipHardGoalCheck(boolean skipHardGoalCheck) {
+        this.skipHardGoalCheck = skipHardGoalCheck;
     }
 
     @Description("Enable the verbose mode for the JSON string describing the optimization result in the related status")
